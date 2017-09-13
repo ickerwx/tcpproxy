b911a7ce034d1e81f7b3872c572d3673f97f9b5f
Adrian Vollmer <adrian.vollmer@syss.de>
2017-09-13 15:54:32 +0200

    added replace module

---
+++ proxymodules/replace.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import re
+
+
+class Module:
+    def __init__(self, incoming=False, options=None):
+        # extract the file name from __file__. __file__ is proxymodules/name.py
+        self.name = __file__.rsplit('/', 1)[1].split('.')[0]
+        self.description = 'Replace text by using regular expressions'
+        self.incoming = incoming  # incoming means module is on -im chain
+        self.find = None  # if find is not None, this text will be highlighted
+        if options is not None:
+            self.search = options['search']
+            self.replace = options['replace']
+
+    def execute(self, data):
+        new_data = re.sub(self.search, self.replace, data)
+        if not new_data == data:
+            print("Replacing '%s' with '%s'" % (self.search, self.replace))
+        return new_data
+
+    def help(self):
+        h = '\tsearch: string that should be replaced\n'
+        h += ('\treplace: value that it should be replaced with')
+        return h
+
+
+if __name__ == '__main__':
+    print 'This module is not supposed to be executed alone!'
