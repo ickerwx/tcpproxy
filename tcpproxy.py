ickerwx/tcpproxy

commit cdb8078dd37c4100f8ba50759dd8906cc77a89e4

marked some arguments as required

tcpproxy.py
@@ -27,18 +27,18 @@ def parse_args():
                                                  'Select modules to handle ' +
                                                  'the intercepted traffic.')
 
+    parser.add_argument('-ti', '--targetip', dest='target_ip', required=True,
+                        help='remote target IP')
+
+    parser.add_argument('-tp', '--targetport', dest='target_port', type=int,
+                        help='remote target port', required=True)
+
     parser.add_argument('-li', '--listenip', dest='listen_ip',
                         default='0.0.0.0', help='IP address to listen for ' +
                         'incoming data')
 
-    parser.add_argument('-ti', '--targetip', dest='target_ip',
-                        help='remote target IP')
-
     parser.add_argument('-lp', '--listenport', dest='listen_port', type=int,
                         default=8080, help='port to listen on')
-
-    parser.add_argument('-tp', '--targetport', dest='target_port', type=int,
-                        help='remote target port')
 
     parser.add_argument('-om', '--outmodules', dest='out_modules',
                         help='comma-separated list of modules to modify data' +

commit ff3ee49daf9a7e29141661fa8c30ed3cd8cc076f

printing module help works
commit cdb8078d broke it when you did not also specify -ti and -tp
which is kind of a stupid requirement

tcpproxy.py
@@ -276,7 +276,7 @@ def log(handle, message, message_only=False):
 
 def main():
     args = parse_args()
-    if args.list is False:
+    if args.list is False and args.help_modules is None:
         if not args.target_ip:
             print 'Target IP is required: -ti'
             sys.exit(6)