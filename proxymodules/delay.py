9814eda52e5fab66fec5f28f068ef62cd31a67b1
René Werner <mail@renewerner.net>
2024-05-29 23:22:20 +0200

    added delay randomization to the delay module

--- proxymodules/delay.py
+++ proxymodules/delay.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os.path as path
 import time
+import random
 
 
 class Module:
@@ -9,19 +10,40 @@ class Module:
         self.name = path.splitext(path.basename(__file__))[0]
         self.description = 'Set delay in passing through the packet, used for simulating various network conditions'
         self.incoming = incoming  # incoming means module is on -im chain
+        self.random = False
         self.seconds = None
+        self.verbose = verbose
         if options is not None:
             if 'seconds' in options.keys():
-                self.seconds = float(options['seconds'])
+                try:
+                    self.seconds = abs(float(options['seconds']))
+                except ValueError:
+                    print(f"Can't parse {options['seconds']} as float")
+                    pass  # leave it set to None
+            if 'random' in options.keys():
+                # set random=true to enable delay randomization
+                self.random = (options['random'].lower() == 'true')
+                if self.random and self.seconds is None:
+                    # set a upper bound of 1s if seconds is not being used, otherwise keep the seconds value
+                    self.seconds = 1.0
 
     def execute(self, data):
-        if self.seconds:
-            time.sleep(self.seconds)
-
+        delay = None
+        if self.random:
+            delay = round(random.uniform(0, self.seconds), 3)  # round to milliseconds
+        else:
+            delay = self.seconds
+        # here delay is either None or a positive float
+        # if the module was instantiated w/o either seconds or random, effectively nothing happens
+        if delay is not None:
+            if self.verbose:
+                print(f"Waiting {delay}s.")
+            time.sleep(delay)
         return data
 
     def help(self):
-        h = '\tseconds: number of seconds you want the packet to be delayed'
+        h = '\tseconds: number of seconds you want the packet to be delayed\n'
+        h += ('\trandom: optional; set to true to randomize the delay between 0 and seconds (default: 1.0s)\n')
         return h
 
 
